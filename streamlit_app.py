461f0aa908f68cdeeb6cdf8105ad0bca59f9c423
NoviyantiDian <noviyantidianpuspita@gmail.com>
2024-12-04 21:18:49 +0700

    update commit

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,39 +3,42 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from pathlib import Path
 
+# Judul aplikasi
 st.title("Analisis Hubungan Antara Kemiskinan dan Stunting di Jawa Barat")
 
+# Display nama-nama pembuat
 st.markdown("""
     <div style="text-align: center;">
-        Disusun oleh :
+        Disusun oleh:
     </div>
     """, 
     unsafe_allow_html=True)
-col1,col2,col3,col4 = st.columns(4)
 
-with col1 :
+col1, col2, col3, col4 = st.columns(4)
+
+with col1:
     st.header("Annisa")
-    st.image("https://scontent.fbdo9-1.fna.fbcdn.net/v/t39.30808-6/468405109_122190413258168129_1314148763243046065_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=O-4oiEGm_jkQ7kNvgHUvoJT&_nc_zt=23&_nc_ht=scontent.fbdo9-1.fna&_nc_gid=Ay4S3WJLlTlymBkUOjPezj4&oh=00_AYDAsksbR7YOAFzWhVp6vTboT4qtJeXe41HaUj6eJJkGyg&oe=6755CBD7", use_container_width=True)
+    st.image("https://via.placeholder.com/150", use_container_width=True)
     if st.button('NRP Annisa'):
-        st.write('220534024')    
+        st.write('220534024')
 
-with col2 :
+with col2:
     st.header("Amanda")
-    st.image("https://scontent.fbdo9-1.fna.fbcdn.net/v/t39.30808-6/468281884_122190412982168129_542957181432111322_n.jpg?_nc_cat=102&ccb=1-7&_nc_sid=833d8c&_nc_ohc=SkeBdY_hbbMQ7kNvgH1xlm7&_nc_zt=23&_nc_ht=scontent.fbdo9-1.fna&_nc_gid=Ag11E5JXrrd7QC2x98GGADE&oh=00_AYAdxI8AVK_NTFfQW8Vic5Y790_AWQ6MfhlD3eUlXEi__w&oe=6755F72D", use_container_width=True)
+    st.image("https://via.placeholder.com/150", use_container_width=True)
     if st.button('NRP Amanda'):
-        st.write('220534025') 
+        st.write('220534025')
 
-with col3 :
+with col3:
     st.header("Widya")
-    st.image("https://scontent.fbdo9-1.fna.fbcdn.net/v/t39.30808-6/468394564_122190413282168129_2674353143231961010_n.jpg?stp=dst-jpg_s600x600&_nc_cat=110&ccb=1-7&_nc_sid=833d8c&_nc_ohc=m_bbxN0rOdgQ7kNvgGQkwDN&_nc_zt=23&_nc_ht=scontent.fbdo9-1.fna&_nc_gid=A7ySlzRUjfMkZtaQxHBjRdu&oh=00_AYDEXtBK-CrLv0lYz07n-erV2cP40mKf3vvsAlja-d-X8A&oe=6755E094", use_container_width=True)
+    st.image("https://via.placeholder.com/150", use_container_width=True)
     if st.button('NRP Widya'):
-        st.write('220534026') 
+        st.write('220534026')
 
-with col4 :
+with col4:
     st.header("Noviyanti")
-    st.image("https://scontent.fbdo9-1.fna.fbcdn.net/v/t39.30808-6/468403667_122190413156168129_8872366674385927362_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=833d8c&_nc_ohc=jmWRtoO_ZW8Q7kNvgEdUcka&_nc_zt=23&_nc_ht=scontent.fbdo9-1.fna&_nc_gid=ARYelPnwHcYNs0AvyNWj_zD&oh=00_AYDWXPonKGarlrA4-S7SU_i0E7avNoqb-WYy8jCzD3iEwg&oe=6755E6F5", use_container_width=True)
+    st.image("https://via.placeholder.com/150", use_container_width=True)
     if st.button('NRP Novi'):
-        st.write('220534028') 
+        st.write('220534028')
 
 # Global file paths
 DATA_1 = Path('data/Jumlah_Kemiskinan.csv')
@@ -73,28 +76,24 @@ else:
     st.write("Data Gabungan (Jumlah per Tahun):")
     st.dataframe(summed_data)
 
-    # Import tambahan
-import matplotlib.pyplot as plt
+    # Plot grafik batang
+    st.write("Grafik Perbandingan Jumlah Penduduk Miskin dan Stunting (2019-2022)")
+    fig, ax = plt.subplots(figsize=(10, 6))
+    x = summed_data['tahun']
+    bar_width = 0.4
 
-# Plot grafik batang
-st.write("Grafik Perbandingan Jumlah Penduduk Miskin dan Stunting (2019-2022)")
-fig, ax = plt.subplots(figsize=(10, 6))
-x = summed_data['tahun']
-bar_width = 0.4
+    # Bar data penduduk miskin
+    ax.bar(x - bar_width / 2, summed_data['jumlah_penduduk_miskin'], bar_width, label='Penduduk Miskin', color='skyblue')
 
-# Bar data penduduk miskin
-ax.bar(x - bar_width / 2, summed_data['jumlah_penduduk_miskin'], bar_width, label='Penduduk Miskin', color='skyblue')
+    # Bar data penduduk stunting
+    ax.bar(x + bar_width / 2, summed_data['jumlah_balita_stunting'], bar_width, label='Penduduk Stunting', color='pink')
 
-# Bar data penduduk stunting
-ax.bar(x + bar_width / 2, summed_data['jumlah_balita_stunting'], bar_width, label='Penduduk Stunting', color='pink')
+    # Menambahkan label dan judul
+    ax.set_xlabel('Tahun')
+    ax.set_ylabel('Jumlah (dalam ribuan/miliar)')
+    ax.set_title('Grafik Perbandingan Penduduk Miskin dan Stunting (2019-2022)')
+    ax.set_xticks(x)
+    ax.legend()
 
-# Menambahkan label dan judul
-ax.set_xlabel('Tahun')
-ax.set_ylabel('Jumlah (dalam ribuan/miliar)')
-ax.set_title('Grafik Perbandingan Penduduk Miskin dan Stunting (2019-2022)')
-ax.set_xticks(x)
-ax.legend()
-
-# Tampilkan grafik di Streamlit
-st.pyplot(fig)
-
+    # Tampilkan grafik di Streamlit
+    st.pyplot(fig)
